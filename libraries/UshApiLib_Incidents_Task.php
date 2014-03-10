e7a08ab63e64cfb49e405142c46911b4a5883133
Robbie Mackay <rm@robbiemackay.com>
2014-03-10 14:55:22 +1300

    Save original incidents array, not just the ORM objects

--- libraries/UshApiLib_Incidents_Task.php
+++ libraries/UshApiLib_Incidents_Task.php
@@ -123,8 +123,13 @@ class UshApiLib_Incidents_Task extends UshApiLib_Task_Base
 								}
 		    			}
 	    			}
-	    			$incident_info_array =array(UshApiLib_Task_Base::INCIDENT_INDEX=>$incident, UshApiLib_Task_Base::CATEGORIES_INDEX=>$categories, 
-	    				UshApiLib_Task_Base::LOCATION_INDEX=>$location, UshApiLib_Task_Base::MEDIA_INDEX=>$medias);
+	    			$incident_info_array =array(
+	    				UshApiLib_Task_Base::INCIDENT_INDEX => $incident,
+	    				UshApiLib_Task_Base::CATEGORIES_INDEX => $categories,
+	    				UshApiLib_Task_Base::LOCATION_INDEX => $location,
+	    				UshApiLib_Task_Base::MEDIA_INDEX => $medias,
+	    				'original' => $inc
+	    			);
 	    			$incidents[$inc[UshApiLib_Task_Base::INCIDENT_INDEX][UshApiLib_Task_Base::INCIDENT_ID_INDEX]] = $incident_info_array;
 	    		
     		}
